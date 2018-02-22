d405b6dc3e6cdaadb781414e190f95a391f44e37
Eulogio Gutierrez <eulogio.gutierrez@digital.hmrc.gov.uk>
2018-02-22 13:01:06 +0000

    changed from upload to continue

--- test/controllers/FileUploadControllerSpec.scala
+++ test/controllers/FileUploadControllerSpec.scala
@@ -252,7 +252,7 @@ class FileUploadControllerSpec extends UnitSpec with WithFakeApplication with I1
       val rasSession = RasSession(userChoice, memberName, memberNino, memberDob, ResidencyStatusResult("", None, "", "", "", "", ""), None, Some(Envelope("existingEnvelopeId123")))
       when(mockSessionService.fetchRasSession()(Matchers.any(), Matchers.any())).thenReturn(Future.successful(Some(rasSession)))
       val result = TestFileUploadController.get().apply(fakeRequest)
-      doc(result).getElementById("upload-button").text shouldBe Messages("upload")
+      doc(result).getElementById("continue-button").text shouldBe Messages("continue")
     }
 
     "contain empty file error if present in session cache" in {
