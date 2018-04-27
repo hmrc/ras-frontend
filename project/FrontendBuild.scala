17ab131314539cb3c2a6d20d7bd656cd11a8f672
Eulogio Gutierrez <eulogio.gutierrez@digital.hmrc.gov.uk>
2018-04-27 15:20:37 +0100

    http-caching-client to 7.1.0

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -16,7 +16,7 @@ object FrontendBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "frontend-bootstrap" % "8.6.0",
     "uk.gov.hmrc" %% "play-partials" % "6.1.0",
     "uk.gov.hmrc" %% "auth-client" % "1.0.0",
-    "uk.gov.hmrc" %% "http-caching-client" % "7.0.0",
+    "uk.gov.hmrc" %% "http-caching-client" % "7.1.0",
     "uk.gov.hmrc" %% "play-ui" % "7.13.0"
   )
 
