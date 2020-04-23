32961732d03915ab57bd6a3cdb4d513dc8206391
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2020-04-23 13:42:12 +0100

    Changes for US DDCE-451

--- app/controllers/MemberNinoController.scala
+++ app/controllers/MemberNinoController.scala
@@ -18,37 +18,45 @@ package controllers
 
 import config.ApplicationConfig
 import connectors.ResidencyStatusAPIConnector
-import forms.MemberNinoForm.form
+import forms.{MemberNinoForm => form}
 import javax.inject.Inject
 import models.ApiVersion
 import play.api.Logger
 import play.api.mvc.{Action, AnyContent}
 import services.{SessionService, ShortLivedCache}
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.audit.DefaultAuditConnector
 import uk.gov.hmrc.play.bootstrap.auth.DefaultAuthConnector
 
 import scala.concurrent.Future
 
 class MemberNinoController @Inject()(val authConnector: DefaultAuthConnector,
-																		 val residencyStatusAPIConnector: ResidencyStatusAPIConnector,
-																		 val connector: DefaultAuditConnector,
-																		 val shortLivedCache: ShortLivedCache,
-																		 val sessionService: SessionService,
-																		 implicit val appConfig: ApplicationConfig
-																		) extends RasResidencyCheckerController with PageFlowController {
+                                     val residencyStatusAPIConnector: ResidencyStatusAPIConnector,
+                                     val connector: DefaultAuditConnector,
+                                     val shortLivedCache: ShortLivedCache,
+                                     val sessionService: SessionService,
+                                     implicit val appConfig: ApplicationConfig
+                                    ) extends RasResidencyCheckerController with PageFlowController {
 
-	lazy val apiVersion: ApiVersion = appConfig.rasApiVersion
+  lazy val apiVersion: ApiVersion = appConfig.rasApiVersion
 
-def get(edit: Boolean = false): Action[AnyContent] = Action.async {
+  def getFullName()(implicit hc: HeaderCarrier): Future[String] = {
+    sessionService.fetchRasSession() map {
+      case Some(session) => session.name.firstName.capitalize + " " + session.name.lastName.capitalize
+      case _ => Messages("member")
+    }
+  }
+
+  def get(edit: Boolean = false): Action[AnyContent] = Action.async {
     implicit request =>
       isAuthorised.flatMap {
         case Right(_) =>
           sessionService.fetchRasSession() map {
             case Some(session) =>
               val name = session.name.firstName.capitalize + " " + session.name.lastName.capitalize
-              Ok(views.html.member_nino(form.fill(session.nino), name, edit))
+              Ok(views.html.member_nino(form(Some(name)).fill(session.nino), name, edit))
             case _ =>
-              Ok(views.html.member_nino(form, Messages("member"), edit))
+              Ok(views.html.member_nino(form(), Messages("member"), edit))
           }
         case Left(resp) =>
           Logger.warn("[NinoController][get] user Not authorised")
@@ -60,30 +68,27 @@ def get(edit: Boolean = false): Action[AnyContent] = Action.async {
     implicit request =>
       isAuthorised.flatMap {
         case Right(userId) =>
-          form.bindFromRequest.fold(
-            formWithErrors => {
-              Logger.warn("[NinoController][post] Invalid form field passed")
-              sessionService.fetchRasSession() map {
-                case Some(session) =>
-                  val name = session.name.firstName.capitalize + " " + session.name.lastName.capitalize
-                  BadRequest(views.html.member_nino(formWithErrors, name, edit))
-                case _ =>
-                  BadRequest(views.html.member_nino(formWithErrors, Messages("member"), edit))
+          getFullName() flatMap { name =>
+            form(Some(name)).bindFromRequest.fold(
+              formWithErrors => {
+                Logger.warn("[NinoController][post] Invalid form field passed")
+                Future.successful(BadRequest(views.html.member_nino(formWithErrors, name, edit)))
+              },
+              memberNino => {
+                sessionService.cacheNino(memberNino.copy(nino = memberNino.nino.replaceAll("\\s", ""))) flatMap {
+                  case Some(session) =>
+                    if (edit) {
+                      submitResidencyStatus(session, userId)
+                    } else {
+                      Future.successful(Redirect(routes.MemberDOBController.get()))
+                    }
+                  case _ => Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage()))
+                }
               }
-            },
-            memberNino => {
-              sessionService.cacheNino(memberNino.copy(nino = memberNino.nino.replaceAll("\\s", ""))) flatMap {
-                case Some(session) =>
-									if (edit) {
-										submitResidencyStatus(session, userId)
-									} else {
-										Future.successful(Redirect(routes.MemberDOBController.get()))
-									}
-								case _ => Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage()))
-              }
-            }
-          )
-        case Left(resp) =>
+            )
+          }
+        case Left(resp)
+        =>
           Logger.warn("[NinoController][post] user Not authorised")
           resp
       }
