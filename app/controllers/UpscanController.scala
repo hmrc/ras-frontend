312b56371391317b9f03d76f9ec597e45b6b02ef
zmdnoor <71127865+zmdnoor@users.noreply.github.com>
2024-05-03 16:11:41 +0100

    DDCE-4709 Latest changes

--- app/controllers/UpscanController.scala
+++ app/controllers/UpscanController.scala
@@ -32,60 +32,53 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class UpscanController @Inject()(upscanInitiateConnector: UpscanInitiateConnector,
-                                     val authConnector: DefaultAuthConnector,
-                                     val filesSessionService: FilesSessionService,
-                                     val sessionService: SessionCacheService,
-                                     val mcc: MessagesControllerComponents,
-                                     implicit val appConfig: ApplicationConfig,
-                                     fileUploadView: views.html.file_upload,
-                                     fileUploadSuccessView: views.html.file_upload_successful,
-                                     cannotUploadAnotherView: views.html.cannot_upload_another_file
-																		) extends FrontendController(mcc) with RasController with PageFlowController with Logging {
+                                 val authConnector: DefaultAuthConnector,
+                                 val filesSessionService: FilesSessionService,
+                                 val sessionService: SessionCacheService,
+                                 val mcc: MessagesControllerComponents,
+                                 implicit val appConfig: ApplicationConfig,
+                                 fileUploadView: views.html.file_upload,
+                                 fileUploadSuccessView: views.html.file_upload_successful,
+                                 cannotUploadAnotherView: views.html.cannot_upload_another_file
+                                ) extends FrontendController(mcc) with RasController with PageFlowController with Logging {
 
-	implicit val ec: ExecutionContext = mcc.executionContext
+  implicit val ec: ExecutionContext = mcc.executionContext
 
   def get: Action[AnyContent] = Action.async { implicit request =>
-    isAuthorised().flatMap { result =>
-      if (result.isRight) {
-        val userId = result.right.get
+    isAuthorised().flatMap {
+      case Left(resp) =>
+        logger.warn("[UpscanController][get] user not authorised")
+        resp
+      case Right(userId) =>
         val fileUploadUrl: Future[Option[UpscanInitiateResponse]] = createFileUploadUrl(userId)
         val sessionFetchResult = sessionService.fetchRasSession()
-        sessionFetchResult.flatMap { sessionOption =>
-          if (sessionOption.isDefined) {
-            val session = sessionOption.get
-            if (session.isInstanceOf[RasSession]) {
-              val rasSession = session.asInstanceOf[RasSession]
-              processRasSession(rasSession, userId, fileUploadUrl)
-            } else {
-              redirectWithNoRasSession(userId)
+        sessionFetchResult.flatMap {
+          case Some(session) =>
+            session match {
+              case rasSession: RasSession =>
+                processRasSession(rasSession, userId, fileUploadUrl)
+              case _ =>
+                redirectWithNoRasSession(userId)
             }
-          } else {
+          case None =>
             redirectWithNoRasSession(userId)
-          }
         }.recover {
           case e: Throwable =>
             logger.error(s"[UpscanController][get] failed to fetch ras session for userId ($userId) - $e")
             Redirect(routes.ErrorController.renderGlobalErrorPage)
         }
-      } else {
-        val resp = result.left.get
-        logger.warn("[UpscanController][get] user not authorised")
-        resp
-      }
     }
   }
 
   def redirectWithNoRasSession(userId: String)(implicit request: MessagesRequest[AnyContent]): Future[Result] = {
     val fileUploadUrlFuture = createFileUploadUrl(userId)
-    fileUploadUrlFuture.flatMap { urlOption =>
-      if (urlOption.isDefined) {
-        val url = urlOption.get
+    fileUploadUrlFuture.flatMap {
+      case Some(url) =>
         logger.info(s"[UpscanController][get] stored new reference successfully for userId ($userId)")
         Future.successful(Ok(fileUploadView(url, "")))
-      } else {
+      case None =>
         logger.error(s"[UpscanController][get] failed to obtain a form url using new reference for userId ($userId)")
         Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
-      }
     }
   }
 
@@ -94,12 +87,14 @@ class UpscanController @Inject()(upscanInitiateConnector: UpscanInitiateConnecto
                          userId: String,
                          fileUploadUrl: Future[Option[UpscanInitiateResponse]])(implicit hc: HeaderCarrier, request: MessagesRequest[AnyContent]): Future[Result] = {
 
-    filesSessionService.isFileInProgress(userId).flatMap {
-      case true =>
+    filesSessionService.isFileInProgress(userId).flatMap { inProgress =>
+        if (inProgress) {
         logger.info(s"[UpscanController][get] a file is still processing for userId ($userId) " +
           s"so another could not be uploaded")
         Future.successful(Redirect(routes.UpscanController.uploadInProgress))
-      case _ => fileUploadUrl.flatMap(url => processFileUploadUrl(url, session, userId))
+      } else {
+        fileUploadUrl.flatMap(url => processFileUploadUrl(url, session, userId))
+      }
     }
   }
 
@@ -107,32 +102,34 @@ class UpscanController @Inject()(upscanInitiateConnector: UpscanInitiateConnecto
                             url: Option[UpscanInitiateResponse],
                             session: RasSession,
                             userId: String)(implicit request: MessagesRequest[AnyContent]): Future[Result] = {
-    if (url.isDefined) {
-      val value = url.get
-      val error = extractErrorReason(session.uploadResponse)
-      if (error == "upload.failed.error") {
-        sessionService.cacheUploadResponse(UploadResponse("", None)).map {
-          case Some(_) =>
-            Redirect(routes.ErrorController.renderProblemUploadingFilePage)
+    url match {
+      case Some(value) =>
+        val error = extractErrorReason(session.uploadResponse)
+        error match {
+          case "upload.failed.error" =>
+            sessionService.cacheUploadResponse(UploadResponse("", None)).flatMap {
+              case Some(_) =>
+                Future.successful(Redirect(routes.ErrorController.renderProblemUploadingFilePage))
+              case _ =>
+                logger.error(s"[UpscanController][get] failed to obtain a session for userId ($userId)")
+                Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
+            }
           case _ =>
-            logger.error(s"[UpscanController][get] failed to obtain a session for userId ($userId)")
-            Redirect(routes.ErrorController.renderGlobalErrorPage)
+            sessionService.resetCacheUploadResponse()
+            Future.successful(Ok(fileUploadView(value, error)))
         }
-      } else {
-        sessionService.resetCacheUploadResponse()
-        Future.successful(Ok(fileUploadView(value, error)))
-      }
-    } else {
-      logger.error(s"[UpscanController][get] failed to obtain a form url using existing file reference " +
-        s"for userId ($userId)")
-      Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
+      case None =>
+        logger.error(s"[UpscanController][get] failed to obtain a form url using existing file reference " +
+          s"for userId ($userId)")
+        Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
     }
   }
 
-  def createFileUploadUrl(userId: String)(implicit hc:HeaderCarrier, request: Request[_]) = {
+
+  def createFileUploadUrl(userId: String)(implicit hc: HeaderCarrier, request: Request[_]) = {
     def urlToString(c: Call): String = appConfig.uploadRedirectTargetBase + c.url
 
-    val successRedirectUrl  = controllers.routes.UpscanController.uploadSuccess
+    val successRedirectUrl = controllers.routes.UpscanController.uploadSuccess
     val errorRedirectUrl = controllers.routes.UpscanController.uploadError
 
     (for {
@@ -153,43 +150,39 @@ class UpscanController @Inject()(upscanInitiateConnector: UpscanInitiateConnecto
   }
 
   def uploadSuccess: Action[AnyContent] = Action.async { implicit request =>
-    isAuthorised().flatMap { result =>
-      if (result.isRight) {
-        val userId = result.right.get
-        sessionService.fetchRasSession().flatMap { sessionOption =>
-          if (sessionOption.isDefined) {
-            val session = sessionOption.get
-            if (session.file.isDefined) {
-              val file = session.file.get
-              filesSessionService.createFileSession(userId, file.id).map { creationResult =>
-                if (creationResult) {
-                  logger.info(s"[UpscanController][uploadSuccess] upload has been successful for userId ($userId)")
-                  Ok(fileUploadSuccessView())
-                } else {
-                  logger.error(s"[UpscanController][uploadSuccess] failed to create file session for userId ($userId)")
-                  Redirect(routes.ErrorController.renderGlobalErrorPage)
+    isAuthorised().flatMap {
+      case Right(userId) =>
+        sessionService.fetchRasSession().flatMap {
+          case Some(session) =>
+            session.file match {
+              case Some(file) =>
+                filesSessionService.createFileSession(userId, file.id).map { successfulCreationResult =>
+                  if (successfulCreationResult) {
+                    logger.info(s"[UpscanController][uploadSuccess] upload has been successful for userId ($userId)")
+                    Ok(fileUploadSuccessView())
+                  } else {
+                    logger.error(s"[UpscanController][uploadSuccess] failed to create file session for userId ($userId)")
+                    Redirect(routes.ErrorController.renderGlobalErrorPage)
+                  }
                 }
-              }
-            } else {
-              logger.error(s"[UpscanController][uploadSuccess] no file reference exists in the session for userId ($userId)")
-              Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
+              case None =>
+                logger.error(s"[UpscanController][uploadSuccess] no file reference exists in the session for userId ($userId)")
+                Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
             }
-          } else {
+          case None =>
             logger.error(s"[UpscanController][uploadSuccess] session could not be retrieved for userId ($userId)")
             Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
-          }
         }
-      } else {
-        val resp = result.left.get
+      case Left(resp) =>
         logger.warn("[UpscanController][uploadSuccess] user not authorised")
         resp
-      }
     }
   }
 
+
   def uploadError: Action[AnyContent] = Action.async { implicit request =>
-    isAuthorised().flatMap { result =>
-      if (result.isRight) {
+    isAuthorised().flatMap {
+      case Right(_) =>
         val errorCode: String = request.getQueryString("errorCode").getOrElse("")
         val errorReason: String = request.getQueryString("errorMessage").getOrElse("")
         val errorResponse: UploadResponse = UploadResponse(errorCode, Some(errorReason))
@@ -200,58 +193,54 @@ class UpscanController @Inject()(upscanInitiateConnector: UpscanInitiateConnecto
           case _ =>
             Future.successful(Redirect(routes.ErrorController.renderProblemUploadingFilePage))
         }
-      } else {
-        val resp = result.left.get
+      case Left(resp) =>
         logger.warn("[UpscanController][uploadError] user not authorised")
         resp
-      }
     }
   }
+
 
   def uploadInProgress: Action[AnyContent] = Action.async { implicit request =>
     isAuthorised().flatMap { result =>
-      if (result.isRight) {
-        val userId = result.right.get
-        filesSessionService.fetchFileSession(userId).flatMap {
-          case Some(fileSession) =>
-            if (fileSession.resultsFile.isDefined) {
-              logger.info("[UpscanController][uploadInProgress] redirecting to file ready page")
-              Future.successful(Redirect(routes.ChooseAnOptionController.renderFileReadyPage))
-            } else {
-              logger.info("[UpscanController][uploadInProgress] calling cannot upload another file")
-              Future.successful(Ok(cannotUploadAnotherView()))
-            }
-          case None =>
-            logger.info("[UpscanController][uploadInProgress] redirecting to global error")
-            Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
-        }
-      } else {
-        val resp = result.left.get
-        logger.warn("[UpscanController][uploadInProgress] user not authorised")
-        resp
+      result match {
+        case Right(userId) =>
+          filesSessionService.fetchFileSession(userId).flatMap {
+            case Some(fileSession) =>
+              if (fileSession.resultsFile.isDefined) {
+                logger.info("[UpscanController][uploadInProgress] redirecting to file ready page")
+                Future.successful(Redirect(routes.ChooseAnOptionController.renderFileReadyPage))
+              } else {
+                logger.info("[UpscanController][uploadInProgress] calling cannot upload another file")
+                Future.successful(Ok(cannotUploadAnotherView()))
+              }
+            case None =>
+              logger.info("[UpscanController][uploadInProgress] redirecting to global error")
+              Future.successful(Redirect(routes.ErrorController.renderGlobalErrorPage))
+          }
+        case Left(resp) =>
+          logger.warn("[UpscanController][uploadError] user not authorised")
+          resp
       }
     }
   }
 
-  private def extractErrorReason(uploadResponse: Option[UploadResponse]): String = {
-    if (uploadResponse.isDefined) {
-      val response = uploadResponse.get
-      val errorCode = response.code
-      if (errorCode == "EntityTooLarge") {
-        logger.error("[UpscanController][extractErrorReason] file too large")
-        "file.large.error"
-      } else if (errorCode == "EntityTooSmall") {
-        logger.error("[UpscanController][extractErrorReason] file too small")
-        "file.empty.error"
-      } else if (errorCode.isEmpty) {
-        logger.info("[UpscanController][extractErrorReason] no error code returned")
-        ""
-      } else {
-        logger.error(s"[UpscanController][extractErrorReason] returned error code: $errorCode")
-        "upload.failed.error"
-      }
-    } else {
-      ""
+  private def extractErrorReason(uploadResponse: Option[UploadResponse]): String =
+    uploadResponse match {
+      case Some(response) =>
+        response.code match {
+          case "EntityTooLarge" =>
+            logger.error("[UpscanController][extractErrorReason] file too large")
+            "file.large.error"
+          case "EntityTooSmall" =>
+            logger.error("[UpscanController][extractErrorReason] file too small")
+            "file.empty.error"
+          case "" =>
+            logger.info("[UpscanController][extractErrorReason] no error code returned")
+            ""
+          case errorCode: String =>
+            logger.error(s"[UpscanController][extractErrorReason] returned error code: $errorCode")
+            "upload.failed.error"
+        }
+      case None => ""
     }
-  }
 }
