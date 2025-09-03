7d03bb8a5ff8534cd6ef2f3502e080d3b5c551a0
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2025-09-03 09:55:46 +0100

    DDCE-7216: [RAS]: Fix Jackson Core Vulnerability [ras-frontend]

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.10.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.3.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0",
     "uk.gov.hmrc"       %% "tax-year"                   % "6.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
