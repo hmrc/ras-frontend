3daac7f148ebf950b8b0b532eec482e279832229
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-12-11 11:44:32 +0000

    DDCE-7747 remove research banner

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val bootstrapVersion = "10.4.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.19.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.23.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0",
     "uk.gov.hmrc"       %% "tax-year"                   % "6.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
